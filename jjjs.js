bd591016a90cb546a84fbdf0bd984516ee8e201d
UuCats1 <132372826+UuCats1@users.noreply.github.com>
2023-05-19 06:13:12 +0900

    更新 jjjs.js

--- jjjs.js
+++ jjjs.js
@@ -1,3 +1,7 @@
-function abc(str){
-  alert(str);
-}
+/**
+ * 加了window.onload 后，直接引入js文件即可
+ * 页面资源全部加载完毕后会自动调用window.onload里的回调函数
+ */
+window.addEventListener("load",function(){
+    console.log("我是外部js test1");
+})
