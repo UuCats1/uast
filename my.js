7baed87f6665f1749488fb057ea96b3930999dbd
UuCats1 <132372826+UuCats1@users.noreply.github.com>
2023-05-18 16:22:00 +0900

    my.js

---
+++ my.js
@@ -0,0 +1,27 @@
+<脚本>
+
+函数 chkdiv(除法){
+
+var chkid=document.getElementById(divid);
+
+如果（孩子！=空）
+
+{返回真；}
+
+别的
+
+{返回假；}
+
+}
+
+最后就是显示JS广告代码了
+
+如果（chkdiv（'guangg1'））{
+
+document.getElementById('guangg1').innerHTML= document.getElementById('ggad1').innerHTML;
+
+document.getElementById('ggad1').innerHTML="";
+
+}
+
+</script>的
